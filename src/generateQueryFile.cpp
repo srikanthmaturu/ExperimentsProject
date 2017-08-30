5f039df4ac596ee96d30d947886ce239a863ffee
srikanthmaturu <srikanthmaturu@outlook.com>
2017-08-30 17:17:14 -0500

    fixes

--- src/generateQueryFile.cpp
+++ src/generateQueryFile.cpp
@@ -22,6 +22,48 @@ map<char, int> ar_map = {{0, 'A'},
                         {3, 'T'},
                         {4, 'N'}};
 
+size_t uiLevenshteinDistance(const std::string &s1, const std::string &s2)
+{
+    const size_t m(s1.size());
+    const size_t n(s2.size());
+
+    if( m==0 ) return n;
+    if( n==0 ) return m;
+
+    size_t *costs = new size_t[n + 1];
+
+    for( size_t k=0; k<=n; k++ ) costs[k] = k;
+
+    size_t i = 0;
+    for ( std::string::const_iterator it1 = s1.begin(); it1 != s1.end(); ++it1, ++i )
+    {
+        costs[0] = i+1;
+        size_t corner = i;
+
+        size_t j = 0;
+        for ( std::string::const_iterator it2 = s2.begin(); it2 != s2.end(); ++it2, ++j )
+        {
+            size_t upper = costs[j+1];
+            if( *it1 == *it2 )
+            {
+                costs[j+1] = corner;
+            }
+            else
+            {
+                size_t t(upper<corner?upper:corner);
+                costs[j+1] = (costs[j]<t?costs[j]:t)+1;
+            }
+
+            corner = upper;
+        }
+    }
+
+    size_t result = costs[n];
+    delete [] costs;
+
+    return result;
+}
+
 void load_sequences(string sequences_file, vector<string>& sequences){
     ifstream input_file(sequences_file, ifstream::in);
 
@@ -54,14 +96,15 @@ void makeQueriesFile(string sequences_file, uint64_t number_of_queries){
 
         for(uint64_t j = i * step_size; j < step_end; j++){
             string query = sequences[rand()%data_size];
-            //cout << "O: " << query << " -> ";
+            string original = query;
+            //cout << "O: " << original << " -> ";
             uint64_t query_size = query.size();
             for(uint64_t k = 0; k < i + 1; k++){
                 uint64_t pos = rand()%query_size;
                 uint64_t replace_character = rand()%4;
                 query[pos] = ar_map[replace_character];
             }
-            //cout << "M: " << query << " " << i+1 << endl;
+            //cout << "M: " << query << " " << i+1 << " " << uiLevenshteinDistance(query, original)<< endl;
             queries_file << query << endl;
         }
     }
